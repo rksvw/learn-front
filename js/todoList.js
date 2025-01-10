73d27774610119f6ae6f1053b4f1dbc537e72eaf
rksvw <ritik.vw98@gmail.com>
2025-01-10 09:58:06 +0530

    add the js functionality to add dynamic todoList.

--- js/todoList.js
+++ js/todoList.js
@@ -8,10 +8,6 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        // Creating the ( X ) sign to remove the task
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
     }
 
     inputBox.value = "";
