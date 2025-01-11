f3f9ea36c74c682d5ac30e84c7606b603d839713
rksvw <ritik.vw98@gmail.com>
2025-01-11 20:05:27 +0530

    for specific city weather update write some document selector in js

--- js/weatherScript.js
+++ js/weatherScript.js
@@ -1,9 +1,20 @@
 let apiKey = secretKey();
 let apiUrl = weatherUrl();
-const checkWeather = async () => {
-    const res = await fetch(apiUrl +`&appid=${apiKey}`);
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const checkWeather = async (city) => {
+    const res = await fetch(apiUrl + city +`&appid=${apiKey}`);
     var data = await res.json();
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
