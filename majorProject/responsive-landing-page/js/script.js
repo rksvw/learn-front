4efd2cefba2196464ee3cfa9a84a4bcb2116680c
rksvw <ritik.vw98@gmail.com>
2025-01-18 12:44:09 +0530

    header and main-home page is created

--- majorProject/responsive-landing-page/js/script.js
+++ majorProject/responsive-landing-page/js/script.js
@@ -15,3 +15,24 @@ if(navClose){
         navMenu.classList.remove("show-menu");
     })
 }
+
+// remove menu mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// change background header
+const scrollHeader = () => {
+    const header = document.getElementById("header");
+    if(this.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    } else {
+        header.classList.remove('scroll-header')
+    }
+}
+window.addEventListener("scroll", scrollHeader);
