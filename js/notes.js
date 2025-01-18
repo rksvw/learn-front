00707d80b0b6edffcdfb213c49f8caf4217bed91
rksvw <ritik.vw98@gmail.com>
2025-01-18 10:47:25 +0530

    problem solve now the notes are saving in localStorage

--- js/notes.js
+++ js/notes.js
@@ -2,40 +2,44 @@ const notesContainer = document.querySelector(".notesContainer");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".inputBox");
 
-// Storing the html-data & inner-text
-const updateStorage = () => {
-  localStorage.setItem("notes", notesContainer.innerHTML);
-};
 // Showing the stored the notes
 const showNotes = () => {
   notesContainer.innerHTML = localStorage.getItem("notes");
 };
-showNotes();
+
+// Storing the html-data & inner-text
+const updateStorage = () => {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+};
 
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
   inputBox.className = "inputBox";
-  inputBox.setAttribute("contenteditable", "true");
+  inputBox.setAttribute("contenteditable", "true")
   img.src = "../img/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
-  updateStorage();
+
 });
 
 // Delete Notes
-notesContainer.addEventListener("click", (e) => {
-  if (e.target.tagName === "IMG") {
-    e.target.parentElement.remove();
-    updateStorage();
-  } else if (e.target.tagName === "P") {
-    notes = document.querySelectorAll(".inputBox");
-    notes.forEach(function(nt){
-      nt.onKeyup = () => {
-        updateStorage();
-      };
-    });
+notesContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "IMG") {
+      e.target.parentElement.remove();
+      updateStorage();
+    } else if (e.target.classList.contains("inputBox")) {
+      notes = document.querySelectorAll(".inputBox");
+      notes.forEach(nt => {
+        nt.onkeyup = ()=>{
+          updateStorage();
+        }
+      })
+    }
   }
-});
+);
+
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
@@ -43,3 +47,6 @@ document.addEventListener("keydown", (event) => {
     event.preventDefault();
   }
 });
+
+
+showNotes();
