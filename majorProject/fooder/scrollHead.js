1927782813b6225c8e3697393f0b626d4e679c98
rksvw <ritik.vw98@gmail.com>
2025-02-06 22:03:55 +0530

    create both header for scroll and no-scroll

--- majorProject/fooder/scrollHead.js
+++ majorProject/fooder/scrollHead.js
@@ -1,9 +1,11 @@
 const scrollHeader = () => {
   const header = document.getElementById("head");
-  if (this.scrollY >= 10) {
+  if (this.scrollY >= 5) {
     header.classList.add('scroll-header');
+    header.classList.remove('no-scroll');
   } else {
     header.classList.remove('scroll-header');
+    header.classList.add('no-scroll');
   }
 };
 
